91bcc7635a4c5f161f0e05f0998c3be927bbce11
Sonam Wangchu <swangchu79@gmail.com>
2022-03-05 22:17:04 +0600

    Create commercial_example_2_general.py

---
+++ class_X/PurchasingDecision/commercial_example_2_general.py
@@ -0,0 +1,25 @@
+"""
+Write a python program for the situation given below:
+
+A compound bow is sold for Nu 45,000. Determine the percent markup if the 
+shop owner paid a cost price of Nu 15,000.
+
+Get the selling price and cost price from the user.
+
+"""
+print("PERCENTAGE MARKUP CALCULATOR")
+
+print("when selling price and cost price entered")
+
+print("********************************************")
+
+selling_price = float(input("Selling price of a compound bow is Nu."))
+
+cost_price = float(input("Cost price of a compound bow is Nu."))
+
+markup = selling_price - cost_price
+
+markup_percent = ( markup / cost_price ) * 100
+
+
+print("The percentage markup is ",markup_percent,"%")
