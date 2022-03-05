87ffc28e83884b792a38882046fc924dd2856f9a
Sonam Wangchu <swangchu79@gmail.com>
2022-03-05 22:16:27 +0600

    Create commercial_example_2.py

---
+++ class_X/PurchasingDecision/commercial_example_2.py
@@ -0,0 +1,18 @@
+"""
+Write a python program for the situation given below:
+
+A compound bow is sold for Nu 45,000. Determine the percent markup if the 
+shop owner paid a cost price of Nu 15,000.
+
+"""
+
+selling_price = 45000
+
+cost_price = 15000
+
+markup = selling_price - cost_price
+
+markup_percent = ( markup / cost_price ) * 100
+
+
+print("The percentage markup is ",markup_percent,"%")
